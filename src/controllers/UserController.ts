b445a1321df43cc967b7b290168d800ba5e809b0
Art0r <arthur0139@gmail.com>
2021-02-25 22:19:05 -0300

    melhoras no createUser

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,31 +11,59 @@ type User = {
 
 class UserController {
 
-    async getUsers (req : Request, res : Response) {
-        res.send(await UserModel.find({}));
-    }
-
     async createUser(req : Request, res : Response) {
         const { name, age } = req.body;
+
+        const verifyUser = await UserModel.find({name: name});
+        const exists : Boolean = verifyUser.length > 0;
+
         try {
-
-            const user : User = {
-                name: name,
-                age: age
-            } 
-
-            const newUser = new UserModel(user);
-            await newUser.save();
-
-            res.status(201);
-            res.json({message: "User created!", user: newUser});
-
-
+            if (name != undefined && age != undefined){
+                if (!exists){
+                    const user : User = {
+                        name: name,
+                        age: age
+                    } 
+        
+                    const newUser = new UserModel(user);
+                    await newUser.save();
+        
+                    res.status(201);
+                    res.json({message: "User created!", user: newUser});
+                } else {
+                    res.status(400);
+                    res.json({err: "User already exists!"});
+                }
+            } else {
+                res.status(400);
+                res.json({err: "Name or age not inserted!"});
+            }             
         } catch (err){
-
             res.status(400);
-            res.json({err: err.message})
+            res.json({err: err.message});
         }
+    }
+
+    async getUsers (req : Request, res : Response) {
+        try {
+            const arr = await UserModel.find({});
+
+            if (arr.length <= 0){
+                res.status(404);
+                res.json({err: "Não há nenhum usuário para ser retornado"});
+            } else res.send(arr);
+            
+        } catch (err) {
+            res.status(400);
+            res.json({err: err.message});
+        }
+        
+    }
+
+    async getUserById(req : Request, res : Response){
+        const id = req.params.id;
+
+        res.send(await UserModel.find({_id: id}));
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import UserController from './controllers/UserController';
 const router = express.Router();
 
 router.get("/user", UserController.getUsers);
+router.get("/user/:id", UserController.getUserById);
 router.post("/user", UserController.createUser);
 
 export default router;
